7c5e292b8a52a05920da10d27ff0ba63d3ac85dd
Joseph Elizondo <josephelizondo0@gmail.com>
2026-05-03 10:36:01 -0600

    PracticeBasic (Optimizacion-ejercicio 00)

--- practice_basic/00_VocalCount.py
+++ practice_basic/00_VocalCount.py
@@ -1,13 +1,49 @@
 '''Crea un programa en Python que:
-Pida al usuario una palabra o frase.
+Pida al usuario una palabra o frase_usuario.
 Cuente cuántas vocales tiene (a, e, i, o, u).
 Muestre el total de vocales encontradas.'''
 
-frase = input("Escribe tu palabra o frase: ")
-contador = 0
+print("\n*Total de vocales en una frase*")
+print("-" * 46)
 
-for letra in frase:
-    if letra.lower() in ["a", "e", "i", "o", "u"]:
-        contador += 1     
+while True:
+    frase_usuario = input("Escribe tu palabra o frase o (fin) para salir: ")
+
+    if frase_usuario.strip().lower() == "fin":
+        print("\nSaliendo del Programa...\n")
+        break
+
+    if not frase_usuario.strip():
+        print("Error: escribe una frase.\n")
+        continue
     
-print("Contiene", contador, "vocales.")
+    error = False  
+
+    for letra in frase_usuario:
+        if letra != " " and not letra.isalpha():
+            print("Error: solo se admiten letras\n")
+            error = True  
+            break
+    
+    if not error: 
+        print("\n- Analizando...")
+
+        contador = 0
+
+        for letra in frase_usuario:
+            if letra.lower() in ["a", "e", "i", "o", "u"]:
+                contador += 1 
+
+        if contador == 0:
+            print(f"La frase ({frase_usuario}) no contiene vocales.\n")
+            print("-" * 46) 
+        else:
+            print(f"La frase ({frase_usuario}) tiene ({contador}) vocales en total.\n")
+            print("-" * 46)
+
+    else:
+        continue
+
+
+
+
