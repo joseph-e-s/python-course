2ba88f0aa95b34eb72c5fd4716aa98292c379607
Joseph Elizondo <josephelizondo0@gmail.com>
2026-05-04 16:48:21 -0600

    PracticeBasic (Optimizacion-ejercicio 01 y 02)

--- practice_basic/01_NumeroSecreto.py
+++ practice_basic/01_NumeroSecreto.py
@@ -1,20 +1,16 @@
 
 '''Haz un programa que:
-
-Genere un número aleatorio entre 1 y 10
-Le pida al usuario que adivine el número
-Le diga si:
-Es muy alto 📈
-Es muy bajo 📉
-O si le pegó 🎯
-Se repita hasta que adivine'''
+1- Genere un número aleatorio entre 1 y 10
+2- Le pida al usuario que adivine el número
+3- Que le diga diga si: Es muy alto, si es muy bajo o si le atino 
+4- Se repita hasta que adivine'''
 
 num_secreto = 38
 intentos = 0
 
 while True:
     try:
-        num_usuario = int (input("Adivina el numero secreto entre 0-100: "))
+        num_usuario = int(input("Adivina el numero secreto entre 0-100: "))
         
         if num_usuario < 0 or num_usuario > 100:
             print("El número debe estar entre 0 y 100\n")
@@ -25,10 +21,10 @@ while True:
             print("Ese numero es alto, digita uno mas bajo")
             print("Llevas", intentos, "intentos\n")
         elif num_usuario < num_secreto:
-            print("Ese numero es bajo, digita una mas alto")
+            print("Ese numero es bajo, digita uno mas alto")
             print("Llevas", intentos, "intentos\n")
         else: 
-            print("Felicidades lo has adivinado era el", num_secreto,)
+            print("\nFelicidades lo has adivinado era el", num_secreto,)
             print("Lo hiciste en", intentos, "intentos\n")
             break
     except ValueError: 
--- practice_basic/02_TablaMultiplicar.py
+++ practice_basic/02_TablaMultiplicar.py
@@ -1,20 +1,28 @@
 
 '''Haz un programa que:
-Pida un número al usuario
-Muestre la tabla de multiplicar de ese número del 1 al 10'''
+1- Pida un número al usuario
+2- Muestre la tabla de multiplicar de ese número del 1 al 10'''
 
-    
-num_multiplicador = 1
 
 while True:
-    try:
-        num_usuario = int(input("Digite un numero para ver su tabla de multiplicar: "))
+    print("-" * 75)
+    num_usuario = input("Digite un numero para ver su tabla de multiplicar o (fin) para salir: ").strip()
+
+    if not num_usuario:
+        print("Error: solo se permiten numeros o 'fin'.\n")
+        continue
+
+    if num_usuario.lower() == "fin":
+        print("\nSaliendo...\n")
         break
+    
+    try:
+        num_usuario = int(num_usuario)
     except ValueError:
-        print("Solo se admiten numeros, intentalo de nuevo.\n")
-        
-while num_multiplicador <= 10:
-    resultado_multiplicacion = num_usuario * num_multiplicador
-    print(num_usuario, "x", num_multiplicador, "=", resultado_multiplicacion)
-    num_multiplicador += 1  
+        print("Error: solo se permiten numeros o 'fin'.\n")
+        continue
+
+    for num_multiplicador in range(1, 11):
+        resultado = num_usuario * num_multiplicador
+        print(num_usuario, "x", num_multiplicador, "=", resultado)
 
